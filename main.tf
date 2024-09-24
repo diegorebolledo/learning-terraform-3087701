935f20a4da8286b29c01debfbcda8d1a90a353e4
diegorebolledo <48532463+diegorebolledo@users.noreply.github.com>
2024-09-24 15:43:34 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
-  default = true
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -44,7 +40,23 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
-module "alb" {
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
