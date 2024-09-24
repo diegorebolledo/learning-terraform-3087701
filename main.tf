c450284f40449e1f0fd78fddc3c137392eee8e90
diegorebolledo <48532463+diegorebolledo@users.noreply.github.com>
2024-09-24 13:25:18 +0200

    subnet correction

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -42,35 +42,6 @@ resource "aws_instance" "blog" {
 
   tags = {
     Name = "HelloWorld"
-  }
-}
-
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name            = "my-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
-
-  listeners = {
-    port             = 80
-    protocol         = "HTTP" 
-    target_group_key = "ex-instance"
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
-  tags = {
-    Environment = "Development"
   }
 }
 
